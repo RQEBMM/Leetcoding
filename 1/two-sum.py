9b6e3855611facff472f95ded3f06cd4765953d8
Ben McCloskey <benjamin.mccloskey@gmail.com>
2023-08-25 00:26:17 -0400

    init

---
+++ 1/two-sum.py
@@ -0,0 +1,13 @@
+class Solution(object):
+        def twoSum(self, nums, target):
+                    """
+                            :type nums: List[int]
+                                    :type target: int
+                                            :rtype: List[int]
+                                                    """
+                                                            d = {num : idx for (idx, num) in enumerate(nums)}
+                                                                    
+                                                                            for (idx, num) in enumerate(nums):
+                                                                                            new_target = target - num
+                                                                                                        if new_target in d and idx != d[new_target]:
+                                                                                                                            return [idx, d[new_target]]
