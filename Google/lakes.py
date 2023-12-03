aa791ef3264580615abe3605077a1df26b177a80
Benjamin McCloskey <benjamin.mccloskey@gmail.com>
2023-12-03 14:32:05 -0500

    Create lakes.py

---
+++ Google/lakes.py
@@ -0,0 +1,12 @@
+# Imagine an island that is in the shape of a bar graph. When it rains, certain areas of the island fill up with rainwater to form lakes. Any excess rainwater the island cannot hold in lakes will run off the island to the west or east and drain into the ocean.
+
+# Given an array of positive integers representing 2-D bar heights, design an algorithm (or write a function) that can compute the total volume (capacity) of water that could be held in all lakes on such an island given an array of the heights of the bars. Assume an elevation map where the width of each bar is 1.
+
+# Example: Given [1,3,2,4,1,3,1,4,5,2,2,1,4,2,2], return 15 (3 bodies of water with volumes of 1,7,7 yields total volume of 15)
+# See image:
+# https://techdevguide.withgoogle.com/static/images/resources/r-in-25-volume-of-water-1.jpg
+
+# Learning objectives
+
+# This question offers practice with algorithms, data structures, Big-O, defining functions, generalization, efficiency, time and space complexity, and anticipating edge cases.
+
