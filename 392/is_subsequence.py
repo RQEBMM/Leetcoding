25c3c63b8e15e5a4ab6791c0f6b319dbbf3a8636
Benjamin McCloskey <benjamin.mccloskey@gmail.com>
2023-12-13 22:52:12 -0500

    Create is_subsequence.py

---
+++ 392/is_subsequence.py
@@ -0,0 +1,53 @@
+# 392. Is Subsequence
+# Easy
+# 9.1K
+# 487
+# Companies
+
+# Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+# A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+ 
+
+# Example 1:
+
+# Input: s = "abc", t = "ahbgdc"
+# Output: true
+
+# Example 2:
+
+# Input: s = "axc", t = "ahbgdc"
+# Output: false
+
+ 
+
+# Constraints:
+
+#     0 <= s.length <= 100
+#     0 <= t.length <= 104
+#     s and t consist only of lowercase English letters.
+
+ 
+# Follow up: Suppose there are lots of incoming s, say s1, s2, ..., sk where k >= 109, and you want to check one by one to see if t has its subsequence. In this scenario, how would you change your code?
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        if len(s) < 1:
+            return True
+        if len(t) < 1:
+            return False        
+        if s == t:
+            return True
+
+        ptr = 0
+        lookup = s[ptr]
+
+        for c in t:
+            if c == lookup:
+                ptr += 1
+                if ptr >= len(s):
+                    return True
+                lookup = s[ptr]
+        
+        return False
